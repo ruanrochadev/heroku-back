3d512f7d86bc1d0f5bbf9e3298a30e1775b906ad
ruanrochadev <ruanrocha.dev@gmail.com>
2025-07-29 19:05:37 -0300

    configurando AppServiceProvider para produção

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+
+//Para erros de RateLimit, limite de requisições para a API
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Configura limite de requisições para a API
+        $this->configureRateLimiting();
+         if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
+    //Para limitar requisições na API por usuário ou IP
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
